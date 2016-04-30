866ac96fb3e4187a3d863fcf6abfdea59e792532
Ricky Leman <rickyleman@gmail.com>
2016-04-29 22:50:04 -0700

    Finished ch.14

--- react-temp/scripts/main.js
+++ react-temp/scripts/main.js
@@ -125,6 +125,20 @@ var Header = React.createClass({
 });
 
 var Order = React.createClass({
+	renderOrder: function(key) {
+		var fish = this.props.fishes[key];
+		var count = this.props.order[key];
+
+		if (!fish) {
+			return <li key={key}>Sorry, fish no longer available!</li>
+		}
+		return (
+			<li>
+				{count} lbs {fish.name}
+				<span className="price">{h.formatPrice(count * fish.price)}</span>
+			</li>
+		);
+	},
 	render: function() {
 		var orderIds = Object.keys(this.props.order);
 		var total = orderIds.reduce((prevTotal, key)=> {
@@ -142,6 +156,7 @@ var Order = React.createClass({
 			<div className="order-wrap">
 				<h2 className="order-title">Your Order</h2>
 				<ul className="order">
+					{orderIds.map(this.renderOrder)}
 					<li className="total">
 						<strong>Total:</strong> {h.formatPrice(total)}
 					</li>
